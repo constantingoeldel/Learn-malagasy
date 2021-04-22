65515fe9fd57f3792296743464c773910ed532e1
Jacquit <jacquit.aja@onja.org>
2021-04-22 13:03:25 +0300

    creating textInput just for displaying a text

--- src/components/PhaseTextarea/PhraseTextarea.js
+++ src/components/PhaseTextarea/PhraseTextarea.js
@@ -1,14 +1,34 @@
+// components/Task.js
 import * as React from 'react';
-import { SafeAreaView, TextInput } from 'react-native';
+import { SafeAreaView, TextInput, StyleSheet } from 'react-native';
+// import { styles } from '../constants/globalStyles';
 
-export default function MyComponent() {
-  const [phraseTextarea, setPhraseTextarea] = useState("");
+export default function Example({MyText}) {
 
   return (
     <SafeAreaView >
-      <TextInput 
-        // onChangeText={(e) => setPhraseTextarea(e.target.value)} 
-      />
+      <TextInput value={MyText} style={styles.input} editable = {false} />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    // font-family: Inter;
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    color: '#111827',
+    lineHeight: 24,
+    fontSize: 20,
+    paddingTop: 41,
+    paddingBottom: 35,
+    marginLeft: 23,
+    marginRight: 23,
+    marginTop: 15,
+    marginBottom: 37,
+    backgroundColor: '#FFFFFF',
+    borderWidth: 1,
+    borderRadius: 3,
+    textAlign: 'center',
+  }
+})
--- src/components/PhaseTextarea/PhraseTextarea.stories.js
+++ src/components/PhaseTextarea/PhraseTextarea.stories.js
@@ -1,12 +1,12 @@
 
-// import * as React from 'react';
-// import { View } from 'react-native';
+import * as React from 'react';
+import { View } from 'react-native';
 
-// import { storiesOf } from '@storybook/react-native';
+import { storiesOf } from '@storybook/react-native';
 
-// import PhraseTextarea from "./PhraseTextarea";
+import PhraseTextarea from "./PhraseTextarea";
 
 
-// storiesOf('Example', module)
-//   .addDecorator(story => <View>{story()}</View>)
-//   .add('Jacquit', () => <PhraseTextarea MyText= {'My name is Jacquit.'} />)
+storiesOf('Example', module)
+  .addDecorator(story => <View>{story()}</View>)
+  .add('Jacquit', () => <PhraseTextarea MyText= {"roa ambin'ny folo"} />)
