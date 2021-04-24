d0321cd8bdd8b78aaf306b954f5ea3f35a2f8ec4
Jacquit <jacquit.aja@onja.org>
2021-04-24 18:18:46 +0300

    creating the heading section

---
+++ src/components/SectionHeading/SectionHeading.stories.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import { storiesOf } from '@storybook/react-native';
+import SectionHeading from './SectionHeading';
+
+storiesOf('SectionHeading', module)
+  .addDecorator(story => <View>{story()}</View>)
+  .add('Learn Malagasy', () => <SectionHeading text= {'Learn Malagasy'}/>)
