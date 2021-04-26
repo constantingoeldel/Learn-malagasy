143b9a664cb48bccd86a2ab5147541da3c728f39
Jacquit <jacquit.aja@onja.org>
2021-04-26 13:54:26 +0300

    changing some structure of styling the svg

--- src/components/ToolButton/ToolButton.stories.js
+++ src/components/ToolButton/ToolButton.stories.js
@@ -1,6 +1,7 @@
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 import ToolButton from './ToolButton';
+import { StyleSheet } from 'react-native';
 import CenterView from '../../../storybook/stories/CenterView';
 import PlusIcon from '../Icons/plus.svg';
 import Correct  from '../Icons/correct.svg';
@@ -12,26 +13,26 @@ storiesOf('ToolButton', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
   .add('plusbtn', () => (
     <ToolButton onPress={alert('clicked PlusIcon')}>
-      <PlusIcon style={StyleSheet.btn} />
+      <PlusIcon width={14} height={14} fill="#000"/>
     </ToolButton>
   ))
   .add('correctbtn', () => (
     <ToolButton onPress={alert('clicked Correct')}>
-      <Correct style={StyleSheet.btn} />
+      <Correct width={14} height={14} fill="#000"/>
     </ToolButton>
   ))
   .add('doublecorrectbtn', () => (
     <ToolButton onPress={alert('clicked Doublecorrect')}>
-      <Doublecorrect style={StyleSheet.btn} />
+      <Doublecorrect width={14} height={14} fill="#000"/>
     </ToolButton>
   ))
   .add('backbtn', () => (
     <ToolButton onPress={alert('clicked back')}>
-      <Back style={StyleSheet.btn} />
+      <Back width={14} height={14} fill="#000"/>
     </ToolButton>
   ))
   .add('switch', () => (
     <ToolButton onPress={alert('clicked switch')}>
-      <Switch style={StyleSheet.btn} />
+      <Switch width={14} height={14} fill="#000"/>
     </ToolButton>
   ))
