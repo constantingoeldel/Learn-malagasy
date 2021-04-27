508866a16c75d925ff4a1a3c790638e5a06c9b5d
Jacquit <jacquit.aja@onja.org>
2021-04-27 10:49:34 +0300

    fixing functions

--- src/components/ToolButton/ToolButton.stories.js
+++ src/components/ToolButton/ToolButton.stories.js
@@ -1,37 +1,37 @@
-import { storiesOf } from '@storybook/react-native';
+import {storiesOf} from '@storybook/react-native';
 import React from 'react';
 import ToolButton from './ToolButton';
 import CenterView from '../../../storybook/stories/CenterView';
 import PlusIcon from '../Icons/plus.svg';
-import Correct  from '../Icons/correct.svg';
-import Doublecorrect  from '../Icons/doublecorrect.svg';
+import Correct from '../Icons/correct.svg';
+import Doublecorrect from '../Icons/doublecorrect.svg';
 import Back from '../Icons/back.svg';
 import Switch from '../Icons/switchcolor.svg';
 
 storiesOf('ToolButton', module)
-  .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
+  .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
   .add('plusbtn', () => (
-    <ToolButton onPress={alert('clicked PlusIcon')}>
-      <PlusIcon width={14} height={14} fill="#000"/>
+    <ToolButton onPress={() => alert('clicked PlusIcon')}>
+      <PlusIcon width={14} height={14} fill="#000" />
     </ToolButton>
   ))
   .add('correctbtn', () => (
-    <ToolButton onPress={alert('clicked Correct')}>
-      <Correct width={14} height={14} fill="#000"/>
+    <ToolButton onPress={() => alert('clicked Correct')}>
+      <Correct width={14} height={14} fill="#000" />
     </ToolButton>
   ))
   .add('doublecorrectbtn', () => (
-    <ToolButton onPress={alert('clicked Doublecorrect')}>
-      <Doublecorrect width={14} height={14} fill="#000"/>
+    <ToolButton onPress={() => alert('clicked Doublecorrect')}>
+      <Doublecorrect width={14} height={14} fill="#000" />
     </ToolButton>
   ))
   .add('backbtn', () => (
-    <ToolButton onPress={alert('clicked back')}>
-      <Back width={14} height={14} fill="#000"/>
+    <ToolButton onPress={() => alert('clicked back')}>
+      <Back width={14} height={14} fill="#000" />
     </ToolButton>
   ))
   .add('switch', () => (
-    <ToolButton onPress={alert('clicked switch')}>
-      <Switch width={14} height={14} fill="#000"/>
+    <ToolButton onPress={() => alert('clicked switch')}>
+      <Switch width={14} height={14} fill="#000" />
     </ToolButton>
-  ))
+  ));
